6b146a9f20702f58ddf4f4950012fc615d7d9c40
Mike Heyman <mheyman@ITO-KOLOA.dlabs.com>
2019-07-14 15:40:34 -1000

    paypal button added

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -19,7 +19,7 @@ export default class Cart extends Component {
                   <Title name="cart" title="cartty" />
                   <CartColumns />
                   <CartList value={value} />
-                  <CartTotals value={value} />
+                  <CartTotals value={value} history={this.props.history} />
                 </>
               );
             } else {
